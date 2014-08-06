5c39143d7e7f536479af8def28f4a157cf225b9d
lenybernard <leny@appventus.com>
2014-08-06 21:20:17 +0200

    Ciao Manager (use Resolver instead)

--- Entity/WidgetFile.php
+++ Entity/WidgetFile.php
@@ -2,7 +2,7 @@
 namespace Victoire\Widget\FileBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use Victoire\Bundle\CoreBundle\Entity\Widget;
+use Victoire\Bundle\WidgetBundle\Entity\Widget;
 
 /**
  * WidgetFile
